9dd2ae4a8bf1b5b4a382e1be9988c05f6d2d05b3
RamiroHojmanFI <rhojman@fi.uba.ar>
2026-07-04 12:37:42 -0300

    a

--- generar_kml.py
+++ generar_kml.py
@@ -4,12 +4,12 @@ def crearKML(nombreArchivo, orden,coordenadas):
         archivo.write('<?xml version="1.0" encoding="UTF-8"?>')
         archivo.write('<kml xmlns="http://earth.google.com/kml/2.1">')
         archivo.write('<Document>')
-        archivo.write('<name>{nombreArchivo}</name>')
+        archivo.write(f'<name>{nombreArchivo}</name>')
         for v in orden:
             archivo.write('<Placemark>')
-            archivo.write('<name>{v}</name>')
+            archivo.write(f'<name>{v}</name>')
             archivo.write('<Point>')
-            archivo.write('<coordinates>{coordenadas[v][0]},{coordenadas[v][1]}</coordinates>')
+            archivo.write(f'<coordinates>{coordenadas[v][0]},{coordenadas[v][1]}</coordinates>')
             archivo.write('</Point>')
             archivo.write('</Placemark>')
         for i in range(len(orden)-1):
@@ -17,10 +17,10 @@ def crearKML(nombreArchivo, orden,coordenadas):
             sig = orden[i+1]
             archivo.write('<Placemark>')
             archivo.write('<LineString>')
-            archivo.write('<coordinates>{coordenadas[actual][0]},{coordenadas[actual][1]} {coordenadas[sig][0]},{coordenadas[sig][1]}</coordinates>')
+            archivo.write(f'<coordinates>{coordenadas[actual][0]},{coordenadas[actual][1]} {coordenadas[sig][0]},{coordenadas[sig][1]}</coordinates>')
             archivo.write('</LineString>')
             archivo.write('</Placemark>')   
         archivo.write('</Document>')
         archivo.write('</kml>')
 def exportarKML(nombreArchivo, orden, coordenadas, texto):
-    kml = crearKML(nombreArchivo,orden,coordenadas)
+    crearKML(nombreArchivo,orden,coordenadas)
