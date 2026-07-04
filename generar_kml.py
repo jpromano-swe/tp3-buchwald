55da261ad473781c66b3087a41aaa59dfed06f84
Juan <juanpiromanok@gmail.com>
2026-07-04 12:20:15 -0300

    Corregir generacion de KML

--- generar_kml.py
+++ generar_kml.py
@@ -1,28 +1,39 @@
+def _coordenadas_vertice(coordenadas, vertice):
+    latitud, longitud = coordenadas[vertice]
+    return f"{latitud}, {longitud}"
 
-def crearKML(nombreArchivo, orden,coordenadas):
-    with open("{nombreArchivo}.kml", "w", encoding="utf-8") as archivo:
-        archivo.write('<?xml version="1.0" encoding="UTF-8"?>')
-        archivo.write('<kml xmlns="http://earth.google.com/kml/2.1">')
-        archivo.write('<Document>')
-        archivo.write('<name>{nombreArchivo}</name>')
+
+def crearKML(nombreArchivo, orden, coordenadas, texto):
+    with open(nombreArchivo, "w", encoding="utf-8") as archivo:
+        archivo.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        archivo.write('<kml xmlns="http://earth.google.com/kml/2.1">\n')
+        archivo.write("\t<Document>\n")
+        archivo.write(f"\t\t<name>{texto}</name>\n\n")
+
         for v in orden:
-            archivo.write('<Placemark>')
-            archivo.write('<name>{v}</name>')
-            archivo.write('<Point>')
-            archivo.write('<coordinates>{coordenadas[v][0]},{coordenadas[v][1]}</coordinates>')
-            archivo.write('</Point>')
-            archivo.write('</Placemark>')
-        for i in range(len(orden)-1):
+            archivo.write("\t\t<Placemark>\n")
+            archivo.write(f"\t\t\t<name>{v}</name>\n")
+            archivo.write("\t\t\t<Point>\n")
+            archivo.write(f"\t\t\t\t<coordinates>{_coordenadas_vertice(coordenadas, v)}</coordinates>\n")
+            archivo.write("\t\t\t</Point>\n")
+            archivo.write("\t\t</Placemark>\n")
+
+        archivo.write("\n")
+        for i in range(len(orden) - 1):
             actual = orden[i]
-            sig = orden[i+1]
-            archivo.write('<Placemark>')
-            archivo.write('<LineString>')
-            archivo.write('<coordinates>{coordenadas[actual][0]},{coordenadas[actual][1]} {coordenadas[sig][0]},{coordenadas[sig][1]}</coordinates>')
-            archivo.write('</LineString>')
-            archivo.write('</Placemark>')   
-        archivo.write('</Document>')
-        archivo.write('</kml>')
+            siguiente = orden[i + 1]
+            archivo.write("\t\t<Placemark>\n")
+            archivo.write("\t\t\t<LineString>\n")
+            archivo.write(
+                f"\t\t\t\t<coordinates>{_coordenadas_vertice(coordenadas, actual)} "
+                f"{_coordenadas_vertice(coordenadas, siguiente)}</coordinates>\n"
+            )
+            archivo.write("\t\t\t</LineString>\n")
+            archivo.write("\t\t</Placemark>\n")
+
+        archivo.write("\t</Document>\n")
+        archivo.write("</kml>\n")
+
 
 def exportarKML(nombreArchivo, orden, coordenadas, texto):
-    print(texto)
-    kml = crearKML(nombreArchivo,orden,coordenadas)
+    crearKML(nombreArchivo, orden, coordenadas, texto)
