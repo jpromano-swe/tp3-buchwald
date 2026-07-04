8771439806ad40c3aa9c202d68f26610195354ad
Juan <juanpiromanok@gmail.com>
2026-07-04 12:06:40 -0300

    Agregados cambios en mst y generar pajek

--- mst.py
+++ mst.py
@@ -1,26 +1,41 @@
-from tdas import heap
-import generar_pajek
+from grafos import Grafo
+from tdas.heap import Heap
 
-def heap_min(v1, v2):     
-    return v1[2] - v2[2]
 
-def mst_prim(grafo, destino):
+def heap_min(arista1, arista2):
+    return arista2[2] - arista1[2]
+
+
+def mst_prim(grafo):
+    vertices = grafo.obtener_vertices()
+    if len(vertices) == 0:
+        return None
+
     visitados = set()
-    heap = Heap(heap_min) 
-    v = grafo.vertice_aleatorio()
-    visitados.add(v)
-    camino = 0
-    for w in grafo.obtener_adyacente(v):
-        heap.encolar((v, w, grafo.peso_arista(v, w)))   
-    recorrido = Grafo(pesado=True, dirigido=False, vertices_iniciales=grafo.obtener_vertices())
+    heap = Heap(heap_min)
+    origen = vertices[0]
+    visitados.add(origen)
+
+    arbol = Grafo(pesado=True, dirigido=False, vertices_iniciales=vertices)
+    peso_total = 0
+
+    for adyacente in grafo.obtener_adyacente(origen):
+        heap.encolar((origen, adyacente, grafo.peso_arista(origen, adyacente)))
+
     while not heap.esta_vacia():
-        v, w, peso = heap.desencolar()  
-        if w not in visitados:
-            visitados.add(w)
-            recorrido.agregar_arista(v, w, peso)
-            camino += peso
-            for u in grafo.obtener_adyacente(w):
-                if u not in visitados:
-                    heap.encolar((w, u, grafo.peso_arista(w, u)))                
-    generar_pajek.crearPajek(recorrido, destino)
-    return recorrido, camino
+        v, w, peso = heap.desencolar()
+        if w in visitados:
+            continue
+
+        visitados.add(w)
+        arbol.agregar_arista(v, w, peso)
+        peso_total += peso
+
+        for adyacente in grafo.obtener_adyacente(w):
+            if adyacente not in visitados:
+                heap.encolar((w, adyacente, grafo.peso_arista(w, adyacente)))
+
+    if len(visitados) != len(vertices):
+        return None
+
+    return arbol, peso_total
